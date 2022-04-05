70bc990344b25cbd533a3f12e67435e7062b8468
Mahmudxon <mahmudxon98@inbox.ru>
2022-04-05 19:48:09 +0500

    Minor Fix

--- app/src/main/kotlin/uz/mahmudxon/halqa/presentation/ui/chapter_list/ChapterListViewModel.kt
+++ app/src/main/kotlin/uz/mahmudxon/halqa/presentation/ui/chapter_list/ChapterListViewModel.kt
@@ -1,10 +1,13 @@
 package uz.mahmudxon.halqa.presentation.ui.chapter_list
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.onEach
 import uz.mahmudxon.halqa.business.domain.model.Chapter
 import uz.mahmudxon.halqa.business.interactors.story.GetChapterList
+import uz.mahmudxon.halqa.util.TAG
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,7 +17,20 @@ class ChapterListViewModel
 ) {
     val chapters: MutableState<List<Chapter>> = mutableStateOf(ArrayList())
     val loading = mutableStateOf(false)
-    var recipeListScrollPosition = 0
+    var listScrollPosition = 0
 
+
+    fun getChaptersList() {
+        getChapterList.execute().onEach { dataState ->
+            loading.value = dataState.loading
+            dataState.data?.let {
+                chapters.value = it
+            }
+
+            dataState.error?.let {
+                Log.e(TAG, "getChaptersList: $it")
+            }
+        }
+    }
 
 }
